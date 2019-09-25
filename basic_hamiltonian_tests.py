36c7c27af680a9cee0481be02eb14fd9c1729a49
Irfansha <irfansha536@gmail.com>
2019-09-25 13:27:09 +0200

    Updated tests and corrected k_connected function to is_connected.

--- basic_hamiltonian_tests.py
+++ basic_hamiltonian_tests.py
@@ -27,11 +27,14 @@ def min_degree(G):
   return min(nodes_deg)
 
 # Returns number of disjoint components in graph G:
-def k_components(G):
-  return len(nx.k_components(G))
+def is_connected(G):
+  return nx.is_connected(G)
 
 # Returns minimum sum of degrees of non-adjacent nodes:
 def min_degree_sum_non_adjacent_nodes(G):
+  # If complete graph:
+  if (G.size() == len(G.nodes)*(len(G.nodes) - 1)/2):
+    return 2 * (len(G.nodes) - 1)
   # Degree of any vertex of simple graph is atmost (n-1),
   # where n is number of nodes:
   min_sum = 2 * len(G.nodes)
@@ -44,7 +47,7 @@ def min_degree_sum_non_adjacent_nodes(G):
       if (not G.has_edge(node1,node2)):
         if min_sum > G.degree[node1] + G.degree[node2]:
           min_sum = G.degree[node1] + G.degree[node2]
-  assert min_sum < (2 * len(G.nodes)), "min_sum is not updated."
+  #assert min_sum < (2 * len(G.nodes)), "min_sum is not updated."
   return min_sum
 
 # Returns true is graph G is trivially non-hamiltonian:
@@ -53,12 +56,15 @@ def is_trivial_non_hamiltonian(G):
   if min_degree(G) < 2:
     return True
   # Hamiltonian graph must be connected:
-  if k_components(G) > 1:
+  if is_connected(G) > 1:
     return True
   return False
 
 def is_trivial_hamiltonian(G):
-  # A graph with minimum degree atleast n/2 is hamiltonian:
+  # A simple graph with minimum degree atleast n/2 is hamiltonian:
   if min_degree(G) >= len(G.nodes):
+    return True
+  # A simple graph with minimum sum of non-adjadent nodes degree atleast n is hamiltonian:
+  if min_degree_sum_non_adjacent_nodes(G) >= len(G.nodes):
     return True
   return False
