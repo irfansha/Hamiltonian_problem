1866960e49946465e356214d675665bb58a3671c
Irfansha <irfansha536@gmail.com>
2019-09-26 10:30:43 +0200

    Added random non-trivial and non-hamiltonian graph generator function.

--- non_trivial_non_hamiltonian_graph_generator.py
+++ non_trivial_non_hamiltonian_graph_generator.py
@@ -20,3 +20,17 @@ def ntnh_atlas_graphs():
       if not (bh_test.is_hamiltonian_slow(G)):
         ntnh_graphs.append(G)
   return ntnh_graphs
+
+# Returns list of all non-trivial non-hamiltonian graph arguments within seeds range:
+def ntnh_random_graphs(N, E, start_seed, end_seed):
+  ntnh_graphs = []
+  for i in range(start_seed,end_seed+1):
+    G = gn.random_graph_gen(N,E,i)
+    if not (bh_test.is_trivial_non_hamiltonian(G) or bh_test.is_trivial_hamiltonian(G)):
+      if not (bh_test.is_hamiltonian_slow(G)):
+        ntnh_graphs.append([N,E,i])
+  return ntnh_graphs
+
+# An example use of ntnh_random_graphs function:
+# Returns all non-trivial non-hamiltonian graphs arguments:
+# print(ntnh_random_graphs(10,15,0,200))
