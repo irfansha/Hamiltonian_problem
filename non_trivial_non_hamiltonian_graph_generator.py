3fa1854584db8a9afcc83e454da4ef035059213c
Irfansha <irfansha536@gmail.com>
2019-09-26 10:54:30 +0200

    Added non-trivial non-hamiltonian graph generator for binomial graphs.

--- non_trivial_non_hamiltonian_graph_generator.py
+++ non_trivial_non_hamiltonian_graph_generator.py
@@ -31,6 +31,20 @@ def ntnh_random_graphs(N, E, start_seed, end_seed):
         ntnh_graphs.append([N,E,i])
   return ntnh_graphs
 
+# Returns list of all non-trivial non-hamiltonian binomial graph arguments within seeds range:
+def ntnh_random_binomial_graphs(N, p, start_seed, end_seed):
+  ntnh_graphs = []
+  for i in range(start_seed,end_seed+1):
+    G = gn.binomial_random_graph_gen(N,p,i)
+    if not (bh_test.is_trivial_non_hamiltonian(G) or bh_test.is_trivial_hamiltonian(G)):
+      if not (bh_test.is_hamiltonian_slow(G)):
+        ntnh_graphs.append([N,p,i])
+  return ntnh_graphs
+
 # An example use of ntnh_random_graphs function:
 # Returns all non-trivial non-hamiltonian graphs arguments:
 # print(ntnh_random_graphs(10,15,0,200))
+
+# An example use of ntnh_random_binomial_graphs function:
+# Returns all non-trivial non-hamiltonian graphs arguments:
+# print(ntnh_random_binomial_graphs(10,15,0,200))
