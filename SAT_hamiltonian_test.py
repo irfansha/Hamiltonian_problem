2bc6c8d4a1341695398b7d7390d92ab92ce7bd61
Irfansha <irfansha536@gmail.com>
2019-09-26 11:01:58 +0200

    Started new module for SAT hamiltonian test.

---
+++ SAT_hamiltonian_test.py
@@ -0,0 +1,15 @@
+# Irfansha Shaik, 26.09.2019, Aarhus.
+
+'''
+SAT test for hamiltonian cycle.
+'''
+
+# Todos:
+# 1. Function for generating SAT encoding.
+# 2. Function for calling SAT solver and finding satifiablity
+
+def SAT_encoding_generation():
+  return 0
+
+def is_hamiltonian_SAT():
+  return True
