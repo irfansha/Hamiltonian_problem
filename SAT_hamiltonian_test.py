b9977a80daed36dadd037b16bdfebdf9530546fe
Irfansha <irfansha536@gmail.com>
2019-09-26 13:06:49 +0200

    completed SAT test.

--- SAT_hamiltonian_test.py
+++ SAT_hamiltonian_test.py
@@ -4,12 +4,28 @@
 SAT test for hamiltonian cycle.
 '''
 
+import generator as gn
+import naive_encoding as n_en
+import os
+
 # Todos:
-# 1. Function for generating SAT encoding.
-# 2. Function for calling SAT solver and finding satifiablity
+# 1. Add asserts for correctness.
 
-def SAT_encoding_generation():
-  return 0
+def read_first_line(file_name):
+  f = open(file_name,"r")
+  line = f.readline()
+  return line
 
-def is_hamiltonian_SAT():
-  return True
+def is_hamiltonian_SAT(G):
+  file_name = "hamiltonian_"+str(len(G.nodes))+"_"+str(G.size())
+  n_en.encoding(G,file_name)
+  # Assuming tawSolver executable is in the same directory:
+  command = "./tawSolver ./" + file_name + " > stats"
+  os.system(command)
+  os.system("rm ./" + file_name)
+  line = read_first_line("stats")
+  os.system("rm ./stats")
+  line = line.rstrip().rstrip("\n")
+  if (line == "s SATISFIABLE"):
+    return True
+  return False
