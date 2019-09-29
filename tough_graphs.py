c7d895df255d453e89efc8ec2396393c25201c22
Irfansha <irfansha536@gmail.com>
2019-09-29 21:07:05 +0200

    Added approximate toughness computations and added relevant experiments. - Adjusted bound to return even if equal.

--- tough_graphs.py
+++ tough_graphs.py
@@ -26,7 +26,7 @@ def bounded_experiment(lst,bound):
     G_toughness = t.bounded_toughness(G,bound)
     if (G_toughness[0] < bound):
       print("Not tough enough with atmost:")
-      print(G_toughness)
+    print(G_toughness)
 
 # Prints toughness of each graph provided with bounded-toughness degree-heuristic computation:
 def bounded_degree_heuristic_experiment(lst,bound):
@@ -35,4 +35,25 @@ def bounded_degree_heuristic_experiment(lst,bound):
     G_toughness = t.degree_heuristic_toughness(G,bound)
     if (G_toughness[0] < bound):
       print("Not tough enough with atmost:")
-      print(G_toughness)
+    print(G_toughness)
+
+# Prints approximate toughness of each graph with linear heuristic computation:
+def linear_approximate_heuristic_experiment(lst):
+  for g_seed in lst:
+    G = gn.binomial_random_graph_gen(g_seed[0],g_seed[1],g_seed[2])
+    G_toughness = t.linear_approx_heuristic_toughness(G)
+    print(G_toughness)
+
+# Prints approximate toughness of each graph with random tries of heuristic computation:
+def random_approximate_heuristic_experiment(lst,n_tries):
+  for g_seed in lst:
+    G = gn.binomial_random_graph_gen(g_seed[0],g_seed[1],g_seed[2])
+    G_toughness = t.random_approx_heuristic_toughness(G,n_tries)
+    print(G_toughness)
+
+# Prints approximate toughness of each graph with improved random tries of heuristic computation with bounds:
+def improved_random_approximate_experiment(lst,n_tries,bound):
+  for g_seed in lst:
+    G = gn.binomial_random_graph_gen(g_seed[0],g_seed[1],g_seed[2])
+    G_toughness = t.improved_random_approx_toughness(G,n_tries,bound)
+    print(G_toughness)
--- toughness.py
+++ toughness.py
@@ -5,37 +5,102 @@ Toughness for undirected graphs.
 '''
 
 # Todos:
-# 1. Improve computation using naive_degree_heuristic_toughness.
-# 2. Improve computation using properties of graphs.
-# 3. Use SAT/QBF for efficient computation.
+# 1. Improve computation using properties of graphs.
+# 2. Use SAT/QBF for efficient computation.
 
 import networkx as nx
 import math
+import random
 from itertools import combinations
+from itertools import permutations
 
 # Returns the toughness of the graph G with cut_set:
 def local_toughness(cut_set,G):
   for v in cut_set:
     G.remove_node(v)
+  # Compute number of connected components:
   n = nx.number_connected_components(G)
   if (n != 1):
     return len(cut_set)/n
   else:
     return math.inf
 
+# Returns approximate toughness with only upper bounds guarantee:
+def linear_approx_heuristic_toughness(G):
+  sorted_list = sorted(G.degree, key=lambda x: x[1], reverse=True)
+  sorted_nodes = []
+  # Sorted node-list with descending degree:
+  for item in sorted_list:
+    sorted_nodes.append(item[0])
+  min_toughness = math.inf
+  min_cut_set = []
+  # Iteratively computing toughness for first i nodes in the sorted list:
+  for i in range(1, len(sorted_nodes)):
+    cut_set = sorted_nodes[:i]
+    l_toughness = local_toughness(cut_set,G.copy())
+    if l_toughness < min_toughness:
+      min_toughness = l_toughness
+      min_cut_set = cut_set
+  return (min_toughness,min_cut_set)
+
+# Returns approximate toughness with only upper bounds guarantee with random heuristics:
+def random_approx_heuristic_toughness(G,n_tries):
+  nodes = list(G.nodes())
+  min_toughness = math.inf
+  min_cut_set = []
+  # Shuffles the nodes list n_tires times:
+  for i in range(0,n_tries):
+    random.shuffle(nodes)
+    # Iteratively computing toughness for first i nodes in the random list:
+    for i in range(1, len(nodes)):
+      cut_set = nodes[:i]
+      l_toughness = local_toughness(cut_set,G.copy())
+      if l_toughness < min_toughness:
+        min_toughness = l_toughness
+        min_cut_set = cut_set
+  return (min_toughness,min_cut_set)
+
+# Returns approximate toughness with only upper bounds guarantee with random heuristics:
+def improved_random_approx_toughness(G,n_tries,bound):
+  sorted_list = sorted(G.degree, key=lambda x: x[1], reverse=True)
+  sorted_nodes = []
+  # Sorted node-list with descending degree:
+  for item in sorted_list:
+    sorted_nodes.append(item[0])
+  min_toughness = math.inf
+  min_cut_set = []
+  random.seed(0)
+  # Shuffles the nodes list n_tires times:
+  for i in range(0,n_tries):
+    # Iteratively computing toughness for first i nodes in the random list:
+    for i in range(1, len(sorted_nodes)):
+      # Returns if the min_toughness is already less than or equal to bound provided:
+      if min_toughness <= bound:
+        return (min_toughness,min_cut_set)
+      cut_set = sorted_nodes[:i]
+      l_toughness = local_toughness(cut_set,G.copy())
+      if l_toughness < min_toughness:
+        min_toughness = l_toughness
+        min_cut_set = cut_set
+    random.shuffle(sorted_nodes)
+  return (min_toughness,min_cut_set)
+
 # Returns toughness of graph G:
 #   - sorts the cut_sets using the total degree of the nodes in descending order.
 def degree_heuristic_toughness(G,bound):
   sorted_list = sorted(G.degree, key=lambda x: x[1], reverse=True)
   sorted_nodes = []
+  # Sorted node-list with descending degree:
   for item in sorted_list:
     sorted_nodes.append(item[0])
   min_toughness = math.inf
   min_cut_set = []
   for cut_size in range(1, len(sorted_nodes)):
+    # Computes the combinations of cut_size:
     cut_set_gen = combinations(sorted_nodes,cut_size)
     for cut_set in cut_set_gen:
-      if min_toughness < bound:
+      # Returns if the min_toughness is already less than or equal to bound provided:
+      if min_toughness <= bound:
         return (min_toughness,min_cut_set)
       l_toughness = local_toughness(cut_set,G.copy())
       if l_toughness < min_toughness:
@@ -44,15 +109,17 @@ def degree_heuristic_toughness(G,bound):
   return (min_toughness,min_cut_set)
 
 # Return toughness of graph G if the toughness is greater than bound
-# else returns a string saying "not tough enough":
+# else returns the min_toughness computed:
 def bounded_toughness(G,bound):
   nodes = G.nodes()
   min_toughness = math.inf
   min_cut_set = []
   for cut_size in range(1, len(nodes)):
+    # Computes the combinations of cut_size:
     cut_set_gen = combinations(nodes,cut_size)
     for cut_set in cut_set_gen:
-      if min_toughness < bound:
+      # Returns if the min_toughness is already less than or equal to bound provided:
+      if min_toughness <= bound:
         return (min_toughness,min_cut_set)
       l_toughness = local_toughness(cut_set,G.copy())
       if l_toughness < min_toughness:
@@ -66,6 +133,7 @@ def naive_toughness(G):
   min_toughness = math.inf
   min_cut_set = []
   for cut_size in range(1, len(nodes)):
+    # Computes the combinations of cut_size:
     cut_set_gen = combinations(nodes,cut_size)
     for cut_set in cut_set_gen:
       l_toughness = local_toughness(cut_set,G.copy())
